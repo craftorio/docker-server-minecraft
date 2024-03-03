b3d4a735ad5bd7e4705299849efdbbb82d4fb3ec
scherepanov <sergey@cherepanov.org.ua>
2024-03-03 20:10:24 +0000

    1.18/1.0.12

--- docker-build-1.18.2-arclight-forge.sh
+++ docker-build-1.18.2-arclight-forge.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MC_DOCKER_IMG="craftorio/docker-server-minecraft"
-MC_SERVER_TAG=arclight-forge-1.18.2-1.0.11 # Minecraft 1.18.2, Forge 40.2.14
+MC_SERVER_TAG=arclight-forge-1.18.2-1.0.12 # Minecraft 1.18.2, Forge 40.2.14
 docker build -f Dockerfile-1182-arclight-forge --squash -t ${MC_DOCKER_IMG}:${MC_SERVER_TAG} \
 --build-arg MC_RELEASE_TAG=1.18.2 --build-arg MC_SERVER_TAG=${MC_SERVER_TAG}  .
 docker push ${MC_DOCKER_IMG}:${MC_SERVER_TAG}
